b1adb260a17fee36c85ad47951a401bc88af454a
Geraskin Roman <nardihhe@gmail.com>
2025-11-27 19:23:33 +0300

    feat(UI): Basic UI

---
+++ frontend/src/ChatBot/ChatHeader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ChatHeader.css';
+
+const ChatHeader = () => {
+    return (
+        <header className="chat-header">
+            <h3 className="header-title">Академический помощник</h3>
+        </header>
+    );
+};
+
+export default ChatHeader;
