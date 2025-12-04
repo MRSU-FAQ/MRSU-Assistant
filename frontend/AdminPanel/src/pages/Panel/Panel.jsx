61f725f2c825789c30c5bfbcf30280706c602974
Geraskin Roman <nardihhe@gmail.com>
2025-12-04 16:58:08 +0300

    feat(admin-panel): implement auth page and questions management interface

--- frontend/AdminPanel/src/pages/Panel/Panel.jsx
+++ frontend/AdminPanel/src/pages/Panel/Panel.jsx
@@ -1,17 +1,29 @@
 import Questions from "./components/Questions/Questions.jsx";
+import eiosIcon from '../../assets/EIOS.png';
+import './panel.css';
 
 export default function Panel() {
-  return(
-    <div className={"panel"}>
-      <div className={"panel-header"}>
-        {/*Кнопку застилизуй, но не делай с ней ничего функционального, буду сам делать*/}
-        <button>Выход</button>
-      </div>
-      <div className={"panel-body"}>
-        <div className={"panel-question"}>
-          <Questions/>
+  return (
+    <div className="panel">
+      <header className="panel-header">
+        <div className="header-left">
+          <img src={eiosIcon} alt="Логотип ЭИОС" className="eios-icon" />
         </div>
-      </div>
+
+        <div className="header-right">
+          <button className="btn-exit">
+            <span className="btn-exit-icon">←</span>
+            <span className="btn-exit-text">Выход</span>
+          </button>
+        </div>
+      </header>
+
+      <main className="panel-body">
+        <div className="container">
+          <h1 className="page-title">Панель администратора</h1>
+          <Questions />
+        </div>
+      </main>
     </div>
-  )
+  );
 }
